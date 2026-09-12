9fd5e792f45c4c8029d9c03e08567c853a197a83
Cherryh4ck <tomas1234567891016@gmail.com>
2026-09-12 17:01:00 -0300

    Añadir stats

--- scripts/player/Stats.gd
+++ scripts/player/Stats.gd
@@ -1,11 +1,12 @@
 extends Node
+@export var vida: int = 100
+@export var max_vida: int = 100
+@export var nivel: int = 1
+@export var exp: int = 0
+@export var exp_level: int = 25
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass 
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
