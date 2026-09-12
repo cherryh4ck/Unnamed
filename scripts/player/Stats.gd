ccfc22ea7c3ae5c72e2212169a1b3465f965ad02
Cherryh4ck <tomas1234567891016@gmail.com>
2026-09-12 17:15:35 -0300

    Prueba de interfaz

--- scripts/player/Stats.gd
+++ scripts/player/Stats.gd
@@ -5,8 +5,13 @@ extends Node
 @export var exp: int = 0
 @export var exp_level: int = 25
 
+@onready var label_vida = $Control/Vida
+@onready var label_nivel = $Control/Nivel
+
 func _ready() -> void:
-	pass 
+	label_vida.text = "Vida: " + str(vida) + "/" + str(max_vida)
+	label_nivel.text = "Nivel: " + str(nivel)
 
 func _process(delta: float) -> void:
-	pass
+	label_vida.text = "Vida: " + str(vida) + "/" + str(max_vida)
+	label_nivel.text = "Nivel: " + str(nivel)
